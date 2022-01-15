e934216b8045a25df76f530aadc788d1b8023505
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2022-01-15 15:29:29 +0000

    Remove demo code

--- out-tsc/src/MemoryCatApp.js
+++ out-tsc/src/MemoryCatApp.js
@@ -1,38 +1,10 @@
-import { __decorate } from "tslib";
 import { LitElement, html, css } from 'lit';
-import { property } from 'lit/decorators.js';
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
 export class MemoryCatApp extends LitElement {
-    constructor() {
-        super(...arguments);
-        this.title = 'My app';
-    }
     render() {
         return html `
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/MemoryCatApp.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <h1>Memory Cats!</h1>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
     }
 }
@@ -55,20 +27,6 @@ MemoryCatApp.styles = css `
       flex-grow: 1;
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
     .app-footer {
       font-size: calc(12px + 0.5vmin);
       align-items: center;
@@ -78,7 +36,4 @@ MemoryCatApp.styles = css `
       margin-left: 5px;
     }
   `;
-__decorate([
-    property({ type: String })
-], MemoryCatApp.prototype, "title", void 0);
 //# sourceMappingURL=MemoryCatApp.js.map
--- src/MemoryCatApp.ts
+++ src/MemoryCatApp.ts
@@ -1,11 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { property } from 'lit/decorators.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
 export class MemoryCatApp extends LitElement {
-  @property({ type: String }) title = 'My app';
-
   static styles = css`
     :host {
       min-height: 100vh;
@@ -25,20 +21,6 @@ export class MemoryCatApp extends LitElement {
       flex-grow: 1;
     }
 
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
-    }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
     .app-footer {
       font-size: calc(12px + 0.5vmin);
       align-items: center;
@@ -52,29 +34,8 @@ export class MemoryCatApp extends LitElement {
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
-
-        <p>Edit <code>src/MemoryCatApp.ts</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+        <h1>Memory Cats!</h1>
       </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
