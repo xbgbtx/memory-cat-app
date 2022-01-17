a63265f428dbcbc4fb78b5403b187f3660bcad83
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2022-01-17 13:44:05 +0000

    Plumbing in xstate context

--- out-tsc/src/MemoryCatAppState.js
+++ out-tsc/src/MemoryCatAppState.js
@@ -1,7 +1,16 @@
-import { createMachine } from 'xstate';
+import { createMachine, assign } from 'xstate';
+const addCatUrl = assign({
+    catUrls: (context, event) => [...context.catUrls, 'foo'],
+});
+function enoughCats(context) {
+    return context.catUrls.length >= 6;
+}
 const memoryCatMachine = createMachine({
     id: 'memory-cat',
     initial: 'welcome',
+    context: {
+        catUrls: [],
+    },
     states: {
         welcome: {
             on: {
@@ -11,6 +20,9 @@ const memoryCatMachine = createMachine({
         fetchCats: {},
         gameplay: {},
     },
+}, {
+    actions: { addCatUrl },
+    guards: { enoughCats },
 });
 export { memoryCatMachine };
 //# sourceMappingURL=MemoryCatAppState.js.map
--- src/MemoryCatAppState.ts
+++ src/MemoryCatAppState.ts
@@ -1,17 +1,38 @@
-import { createMachine } from 'xstate';
+import { createMachine, assign } from 'xstate';
 
-const memoryCatMachine = createMachine({
-  id: 'memory-cat',
-  initial: 'welcome',
-  states: {
-    welcome: {
-      on: {
-        start: { target: 'fetchCats' },
-      },
-    },
-    fetchCats: {},
-    gameplay: {},
-  },
+interface MemoryCatContext {
+  catUrls: Array<string>;
+}
+
+const addCatUrl = assign({
+  catUrls: (context: MemoryCatContext, event) => [...context.catUrls, 'foo'],
 });
+
+function enoughCats(context: MemoryCatContext) {
+  return context.catUrls.length >= 6;
+}
+
+const memoryCatMachine = createMachine<MemoryCatContext>(
+  {
+    id: 'memory-cat',
+    initial: 'welcome',
+    context: {
+      catUrls: [],
+    },
+    states: {
+      welcome: {
+        on: {
+          start: { target: 'fetchCats' },
+        },
+      },
+      fetchCats: {},
+      gameplay: {},
+    },
+  },
+  {
+    actions: { addCatUrl },
+    guards: { enoughCats },
+  }
+);
 
 export { memoryCatMachine };
