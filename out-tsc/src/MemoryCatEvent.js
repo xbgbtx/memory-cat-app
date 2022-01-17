3f75cdf2d2fe58fc3ed98e8e900c8c4331b99ad5
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2022-01-17 18:03:44 +0000

    rm ignored files

--- out-tsc/src/MemoryCatEvent.js
+++
@@ -1,8 +0,0 @@
-export class MemoryCatEvent extends Event {
-    constructor(action) {
-        super('memory-cat-event', { bubbles: true, composed: true });
-        this.action = 'no-action';
-        this.action = action;
-    }
-}
-//# sourceMappingURL=MemoryCatEvent.js.map
