fe98fa8a493bb93106448fa45696bd683d7f318b
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2022-01-22 16:33:47 +0000

    Divide cards among columns

--- src/MemoryCatCardTable.ts
+++ src/MemoryCatCardTable.ts
@@ -7,13 +7,40 @@ export class MemoryCatCardTable extends LitElement {
 
   @property({ type: Array }) cards = [];
 
-  static styles = css``;
+  static styles = css`
+    .card-table {
+      display: grid;
+      grid-template-rows: repeat(auto-fill, 1fr);
+    }
+  `;
 
   render() {
-    return html` <main>${this.renderCards()}</main> `;
+    return html`
+      <main>
+        <div
+          class="card-table"
+          style="grid-template-columns: repeat(${this.numColumns()}, 1fr)"
+        >
+          ${this.renderCards()}
+        </div>
+      </main>
+    `;
+  }
+
+  /**
+   * Number of columns to display cards -- must fill all rows.
+   */
+  numColumns() {
+    const cards = this.cards.length;
+    const allowedCols = [2, 3, 4, 6].filter(n => n < cards / 2);
+
+    return allowedCols.reduce(
+      (prev, next) => (cards % next == 0 ? next : prev),
+      1
+    );
   }
 
   renderCards() {
-    return this.cards.map(url => html`<div>${url}</div>`);
+    return this.cards.map(url => html`<div class="card">${url}</div>`);
   }
 }
