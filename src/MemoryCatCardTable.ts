b84e1cd706cac2db36cc21432be5a0587e96a667
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2022-01-24 11:38:22 +0000

    Card table layout

--- src/MemoryCatCardTable.ts
+++ src/MemoryCatCardTable.ts
@@ -8,9 +8,34 @@ export class MemoryCatCardTable extends LitElement {
   @property({ type: Array }) cards = [];
 
   static styles = css`
+    main {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      font-size: calc(10px + 2vmin);
+      color: #1a2b42;
+      max-width: 960px;
+      margin: 0 auto;
+      text-align: center;
+    }
+
     .card-table {
       display: grid;
       grid-template-rows: repeat(auto-fill, 1fr);
+    }
+
+    .card {
+      aspect-ratio: 1 / 1.6;
+      margin: 8px;
+      border: 5px ridge #8275be;
+      border-radius: 27px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      justify-content: center;
     }
   `;
 
@@ -32,7 +57,7 @@ export class MemoryCatCardTable extends LitElement {
    */
   numColumns() {
     const cards = this.cards.length;
-    const allowedCols = [2, 3, 4, 6].filter(n => n < cards / 2);
+    const allowedCols = [3, 4, 6].filter(n => n <= cards / 2);
 
     return allowedCols.reduce(
       (prev, next) => (cards % next == 0 ? next : prev),
